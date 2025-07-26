7cea4f1d2bed61d92d96d9021939b23a1a22353a
Anass cheab <anassalien123@gmail.com>
2025-07-26 13:21:01 +0200

    aaa

---
+++ POOL_DAYS/C07/ft_strdup.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+char	*ft_strdup(char *src);
+
+char	*ft_strdup(char *src)
+{
+    int i;
+    int len;
+    char    *copy;
+
+    len = 0;
+    while (src[len])
+        len++;
+    
+}
